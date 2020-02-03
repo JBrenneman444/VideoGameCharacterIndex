7dca534ed3d4d791a41c282c75198aac5014301b
LocalPlacesWith <LocalPlacesWith@gmail.com>
2020-02-02 22:35:26 -0800

    final Sunday update

--- main.js
+++ main.js
@@ -1,106 +1,173 @@
 $(()=>{
 
-    /////////////////////////////////////////////////////////////
-    // JSON method
+    // API Key = 8f5e4fe50eb930447615648aa359bbc0ab041539
+        // To filter by name: &filter=name:[NAME]
+        // to format data by JSON or XML: &format=JSON
 
 
-    $('#characterSubmit').on('click', (event)=>{
-        
+        $('form').on('submit', (event)=>{
+
+        var charName = ""
+
+        $('.information').fadeIn(1000)
+
+        var userInput = $('#characterInput').val()
+        console.log("User typed: " + userInput)
+
+        // clear fields
+        $('#alias').html('<b>Alias</b>: <font color=grey>Loading...</font>')
+        $('#birthday').html('<b>Birthday</b>: <font color=grey>Loading...</font>')
+        $('#first').html('<b>First Game</b>: <font color=grey>Loading...</font>')
+        $('#objects').html('<b>Related Objects</b>: <font color=grey>Loading...</font>')
+        $('#friends').html('<b>Friends</b>: <font color=grey>Loading...</font>')
+        $('#enemies').html('<b>Enemies</b>: <font color=grey>Loading...</font>')
+        $('#description').html('<b>Description</b>: <font color=grey>Loading...</font>')
+
+        event.preventDefault();
+
+        ///////////////////////////////////////
+        // FIRST AJAX request
         $.ajax({
-            url:'https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/character/3005-10/?api_key=8f5e4fe50eb930447615648aa359bbc0ab041539&format=JSON',
+            url:`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/characters/?api_key=8f5e4fe50eb930447615648aa359bbc0ab041539&format=JSON&filter=name:${userInput}`,
             type: "GET",
             dataType: "JSON",
             data: {
-            "$limit" : 5
+            "$limit" : 1
             }
 
         }).then(
             (data)=>{
-                console.log(data)
+                
+                // TODO: Loop through various names (if more than one match) and prefer EXACT MATCH
+                var exactMatch
+                var characterArray = data.results
+                    for (i=0;i<characterArray.length;i++) {
+                        if (userInput === characterArray[i].name) {
+                            // then use THESE RESULTS for rest of information
+
+                            break;
+                        } else {
+                            // display FIRST name
+                        }
+                    }
+
+                // TODO: figure out how to make name transition smoother.
+                    // for some reason, it doesn't fade in properly within AJAX call
+                    // POTENTIAL SOLUTION: just have it fade in WHATEVER USER INPUT IS (even if faulty spelling)
+                    
+                $('#characterName').html(data.results[0].name).css('color','ivory').fadeIn(1000)
+
+                $('#alias').html('<b>Alias</b>: ') // TODO: see if can figure out way to identify several aliases
+                $('#birthday').html('<b>Birthday</b>: ')
+                $('#first').html('<b>First Game</b>: ')
+                $('#description').html('<b>Description</b>: ')
+
+                var characterIDNumber = data.results[0].guid
 
                 $('#character-img').text("")
-                var characterImage = data.results.image.screen_large_url
+                var characterImage = data.results[0].image.super_url
                 $('#character-img').css(`background-image`,`url(${characterImage})`)
                 $('#character-img').css("background-size","contain")
                 $('#character-img').css("background-repeat","no-repeat")
                 $('#character-img').css("background-position","center")
 
-                $('#characterName').html(data.results.name)
+
+                /////////////////////////////////////////////////////////////
+                // TODO: If search returns NULL, make it appear as "Unknown"
+                /////////////////////////////////////////////////////////////
 
                 var aliasLabel = $('#alias').text()
-                $('#alias').html('<b>' + aliasLabel + '</b>' + data.results.aliases)
+                $('#alias').html('<b>' + aliasLabel + '</b>' + data.results[0].aliases)
 
                 var bdayLabel = $('#birthday').text()
-                $('#birthday').html('<b>' + bdayLabel + '</b>' + data.results.birthday)
+                $('#birthday').html('<b>' + bdayLabel + '</b>' + data.results[0].birthday)
 
-                var objectsArray = data.results.objects
-                for (i=0;i<objectsArray.length;i++) {
-                    if (i == 0) {
-                        // grab the text
-                        var objectsLabel = $('#objects').html()
-                        // add the text
-                        $('#objects').html('<b>' + objectsLabel + '</b>' + data.results.objects[i].name)
-                    } else if (i > 0) {
-                        // grab the text
-                        var objectsLabel = $('#objects').html()
-                        // add the text
-                        $('#objects').html(objectsLabel + ", " + data.results.objects[i].name)
+                var firstLabel = $('#first').text()
+                $('#first').html('<b>' + firstLabel + '</b>' + data.results[0].first_appeared_in_game.name)
+
+                var descriptionLabel = $('#description').text()
+                $('#description').html('<b>' + descriptionLabel + '</b>' + data.results[0].deck)
+
+                console.log(data)
+
+                ///////////////////////////////////////
+                // SECOND AJAX CALL
+                $.ajax({
+                    url:`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/character/${characterIDNumber}/?api_key=8f5e4fe50eb930447615648aa359bbc0ab041539&format=JSON`, // plug in Character GUID  into template literal
+                    type: "GET",
+                    dataType: "JSON",
+                    data: {
+                    "$limit" : 1
                     }
-                }
+        
+                }).then(
+                    (data)=>{
+                            
+                    console.log(data)
 
-                var friendsArray = data.results.friends
-                for (i=0;i<friendsArray.length;i++) {
-                    if (i == 0) {
-                        // grab the text
-                        var friendsLabel = $('#friends').html()
-                        // add the text
-                        $('#friends').html('<b>' + friendsLabel + '</b>' + data.results.friends[i].name)
-                    } else if (i > 0) {
-                        // grab the text
-                        var friendsLabel = $('#friends').html()
-                        // add the text
-                        $('#friends').html(friendsLabel + ", " + data.results.friends[i].name)
+                    $('#objects').html('<b>Related Objects</b>: ')
+                    $('#friends').html('<b>Friends</b>: ')
+                    $('#enemies').html('<b>Enemies</b>: ')
+
+                    var objectsArray = data.results.objects
+                    for (i=0;i<objectsArray.length;i++) {
+                        if (i == 0) {
+                            // grab the text
+                            var objectsLabel = $('#objects').html()
+                            // add the text
+                            $('#objects').html('<b>' + objectsLabel + '</b>' + data.results.objects[i].name)
+                        } else if (i > 0) {
+                            // grab the text
+                            var objectsLabel = $('#objects').html()
+                            // add the text
+                            $('#objects').html(objectsLabel + ", " + data.results.objects[i].name)
+                        }
                     }
-                }
 
-                var enemiesArray = data.results.enemies
-                for (i=0;i<enemiesArray.length;i++) {
-                    if (i == 0) {
-                        // grab the text
-                        var enemiesLabel = $('#enemies').html()
-                        // add the text
-                        $('#enemies').html('<b>' + enemiesLabel + '</b>' + data.results.enemies[i].name)
-                    } else if (i > 0) {
-                        // grab the text
-                        var enemiesLabel = $('#enemies').html()
-                        // add the text
-                        $('#enemies').html(enemiesLabel + ", " + data.results.enemies[i].name)
+                    var friendsArray = data.results.friends
+                    for (i=0;i<friendsArray.length;i++) {
+                        if (i == 0) {
+                            // grab the text
+                            var friendsLabel = $('#friends').html()
+                            // add the text
+                            $('#friends').html('<b>' + friendsLabel + '</b>' + data.results.friends[i].name)
+                        } else if (i > 0) {
+                            // grab the text
+                            var friendsLabel = $('#friends').html()
+                            // add the text
+                            $('#friends').html(friendsLabel + ", " + data.results.friends[i].name)
+                        }
                     }
-                }
 
-                // var friendsLabel = $('#friends').text()
-                // $('#friends').html('<b>' + friendsLabel + '</b>' + data.results.aliases)
-                
-                
-                //         <li id="enemies"><b>Enemies</b>: </li>
-                //         <li id="summary"><b>Summary</b>: </li>
+                    var enemiesArray = data.results.enemies
+                    for (i=0;i<enemiesArray.length;i++) {
+                        if (i == 0) {
+                            // grab the text
+                            var enemiesLabel = $('#enemies').html()
+                            // add the text
+                            $('#enemies').html('<b>' + enemiesLabel + '</b>' + data.results.enemies[i].name)
+                        } else if (i > 0) {
+                            // grab the text
+                            var enemiesLabel = $('#enemies').html()
+                            // add the text
+                            $('#enemies').html(enemiesLabel + ", " + data.results.enemies[i].name)
+                        }
+                    }
 
-                // RESPONSE is the ONLY child of #document
-                // RESULTS is 6 in the RESPONSE ARRAY
-                // GENDER is 13 in the RESULTS ARRAY
-            },
-            ()=>{
+                    },
+                    ()=>{
 
-                console.log('bad');
-            }
-        );
+                        console.log('bad');
+                    }
+                    ); // end of 2ND AJAX
+
 
     
-    })
+                }) // end of 1ST AJAX
 
-// CODE to CONSIDER for APPENDING to website:
 
-        // $(".main").append('<div class="book"><div class="title">' + $(this).find("Title").text() +   '</div><div class="description">' + $(this).find("Description").text() + '</div><div   class="date">Published ' + $(this).find("Date").text() + '</div></div>');
-        // $(".book").fadeIn(1000);
+
+
+            }) // end of FORM SUBMIT
 
 })
